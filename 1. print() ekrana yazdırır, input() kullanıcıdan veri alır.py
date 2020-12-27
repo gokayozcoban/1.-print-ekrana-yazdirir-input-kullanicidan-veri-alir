e4b1a626c8d016453716726c7fedf281c5af4ffb
gokayozcoban <43710003+gokayozcoban@users.noreply.github.com>
2020-12-27 23:59:58 +0200

    Add files via upload

---
+++ 1. print() ekrana yazdırır, input() kullanıcıdan veri alır.py
@@ -0,0 +1,14 @@
+# 1. Ders (print(), input())
+
+# Ekrana yazdırmak istenilen her şey print() ile ifade edilir:
+print("Gökay")
+#Gökay
+
+# Kullanıcıdan alınmak istenilen veri input() ile ifade edilir:
+# input() parantezlerinin içine ne yazılırsa program kullanıcıya onu sorar.
+input("İsim nedir? ")
+#İsim nedir? 
+input("Soyisim? ")
+#Soyisim?
+
+
